c77bdcc243bbfd00620aa7650cfb9ea978d13ce6
pooya2293 <77061069+pooya2293@users.noreply.github.com>
2021-05-01 22:32:05 +0430

    link data to list DOM

--- 01-birthday-reminder/setup/src/List.js
+++ 01-birthday-reminder/setup/src/List.js
@@ -4,26 +4,20 @@ const List = ({ people }) => {
   return (
     <>
     	{people.map((p)=>{
-    		return (
-    			<Person key={p.id} {...p} />
+    		const {id,name,age,image} = p;
+    		return(
+    			<article key={id} className='person'>
+    				<img src={image} alt={name} />
+    				<div>
+    					<h4>{name}</h4>
+    					<p>{age} years</p>
+    				</div>
+    			</article>
     		)
-    	})
-    	}
+    	})};
     </>
   );
 };
 
-const Person = (props)=>{
-	const {name,age,image} = props
-	return (
-		<article key={id} className="person">
-			<img src={image} alt={name}>
-			<div>
-				<h4>{name}</h4>
-				<p>{age} years</p>
-			</div>
-		</article>
-	)
-}
 
 export default List;
