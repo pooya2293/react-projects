a8c90ff5a8f6c810075b7d77a8f870a8e10fb33e
pooya2293 <77061069+pooya2293@users.noreply.github.com>
2021-05-13 09:50:32 +0430

    change icon when open and close text

--- 04-accordion/setup/src/Question.js
+++ 04-accordion/setup/src/Question.js
@@ -8,7 +8,9 @@ const Question = ({ title,info }) => {
   	<article className="question">
   		<header>
   		<h4>{title}</h4>
-  		<button className="btn" onClick={()=>setShowInfo(!showInfo)}>btn</button>
+  		<button className="btn" onClick={()=>setShowInfo(!showInfo)}>
+  			{showInfo?<AiOutlineMinus />:<AiOutlinePlus />}
+  		</button>
   		</header>
   		{showInfo && <p>{info}</p>}
   	</article>
