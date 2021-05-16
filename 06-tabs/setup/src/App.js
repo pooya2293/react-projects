c087b2e112636c6a205c75c65c40fbfeedea0476
pooya2293 <77061069+pooya2293@users.noreply.github.com>
2021-05-16 19:55:03 +0430

    add date,export it ,import data,link it to DOM,change it by button

--- 06-tabs/setup/src/App.js
+++ 06-tabs/setup/src/App.js
@@ -1,10 +1,50 @@
 import React, { useState, useEffect } from 'react'
 import { FaAngleDoubleRight } from 'react-icons/fa'
+import items from './data'
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tabs-project'
+// const url = 'https://course-api.com/react-tabs-project'
 function App() {
-  return <h2>tabs project setup</h2>
+	const [jobs,setJobs] = useState(items)
+	const [value,setValue] = useState(0)
+	const {company,dates,duties,title} = jobs[value]
+  return (
+  	<section className="section">
+  		<div className="title">
+  			<h2>expereince</h2>
+  			<div className='underline'></div>
+  		</div>
+  		<div className="jobs-center">
+  		{/* btn container */}
+  		<div className="btn-container">
+  			{jobs.map((job,index)=>{
+  				return (
+  					<button key={job.id} 
+  					onClick={()=>setValue(index)}>
+  					{job.company}
+  					</button>
+  				)
+  			})}
+  		</div>
+  		{/* job info */}
+  			<article className="job-info">
+  				<h3>{title}</h3>
+  				<h4>{company}</h4>
+  				<p className="job-date">{dates}</p>
+  				{duties.map((duty,index)=>{
+  					return(
+  						<div key={index} className="job-desc">
+  							<FaAngleDoubleRight className='job-icon' />
+  							<p>{duty}</p>
+  						</div>
+  					)
+  				})}
+  			</article>
+  		</div>
+  	</section>
+  )
 }
 
 export default App
+
+
