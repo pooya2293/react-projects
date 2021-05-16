45f5f811992533974c03d31deca442b54b593a06
pooya2293 <77061069+pooya2293@users.noreply.github.com>
2021-05-16 20:47:03 +0430

    <new project /> problem is add all pic and name with togather

--- 07-slider/setup/src/App.js
+++ 07-slider/setup/src/App.js
@@ -3,7 +3,30 @@ import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
 import { FaQuoteRight } from 'react-icons/fa';
 import data from './data';
 function App() {
-  return <h2>slider project setup</h2>;
+	const[people,setPeople] = useState(data)
+	const[index,setIndex] = useState(0)
+  return (
+  	<section className="section">
+  		<div className="title">
+  			<h2>
+  				<span>/</span>rewies
+  			</h2>
+  		</div>
+  		<div className="section-center">
+  			{people.map((person,personIndex)=>{
+  				const{id,image,name,title,quote} = person;
+  				//more stuff coming up
+
+  				return(
+  					<article key={id}>
+  						<img src={image} alt={name} className='person-img'  />
+  						<h4>{name}</h4>
+  					</article>
+  				)
+  			})}
+  		</div>
+  	</section>
+  )
 }
 
 export default App;
