da784c330c8683bdc2db97f240e0981a378f85d0
pooya2293 <77061069+pooya2293@users.noreply.github.com>
2021-05-17 12:24:23 +0430

    SP: index > people.length

--- 07-slider/setup/src/App.js
+++ 07-slider/setup/src/App.js
@@ -5,12 +5,17 @@ import data from './data';
 function App() {
 	const[people,setPeople] = useState(data)
 	const[index,setIndex] = useState(0)
+
 	 useEffect(() => {
 	 	let lastSlide = people.length - 1;
 	 	if(index < 0 ){
 	 		setIndex(lastSlide)
 	 	}
+	 	if(index > lastSlide){
+	 		setIndex(0)
+	 	}
 	 }, [index,people])
+
   return (
   	<section className="section">
   		<div className="title">
