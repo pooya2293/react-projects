0db5147de83aed4fe2810d7857f8b28e9df1d9f0
pooya2293 <77061069+pooya2293@users.noreply.github.com>
2021-05-17 10:47:46 +0430

    set deafault nextSlide to all - set prevSlide to index -1 and solve problem: : that when index 0-1=-1 show the last of the list item

--- 07-slider/setup/src/App.js
+++ 07-slider/setup/src/App.js
@@ -16,14 +16,30 @@ function App() {
   			{people.map((person,personIndex)=>{
   				const{id,image,name,title,quote} = person;
   				//more stuff coming up
-
+  				let position = 'nextSlide'
+  				if(personIndex === index){
+  					position = 'activeSlide';
+  				}
+  				if(personIndex === index - 1 || index == 0 && personIndex === people.length -1){
+  					position = 'lastSlide'
+  				}
   				return(
-  					<article key={id}>
+  					<article key={id}
+  					 className={position} >
   						<img src={image} alt={name} className='person-img'  />
   						<h4>{name}</h4>
+  						<p className="title">{title}</p>
+  						<p className="text">{quote}</p>
+  						<FaQuoteRight className='icon' />
   					</article>
   				)
   			})}
+  			<button className="prev">
+  				<FiChevronLeft />
+  			</button>
+  			<button className="next">
+  				<FiChevronRight />
+  			</button>
   		</div>
   	</section>
   )
