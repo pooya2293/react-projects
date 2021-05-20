db526266f2bf5cc1fe1caed27101acbe92505a46
pooya2293 <77061069+pooya2293@users.noreply.github.com>
2021-05-20 16:17:31 +0430

    <new project /> , Add count=0 & text=[] const

--- 08-lorem-ipsum/setup/src/App.js
+++ 08-lorem-ipsum/setup/src/App.js
@@ -1,10 +1,14 @@
 import React, { useState } from 'react';
 import data from './data';
 function App() {
-  
-  
+  const [count,setCount] = useState(0) ;
+  const [text , setText] = useState([]);
+
   return (
-  <h2>lorem ipsum project setup</h2>
+  <section className='section-center'>
+  	<h3> Tiered of boring lorem ipsum </h3>
+
+  </section>
     )
 }
 
