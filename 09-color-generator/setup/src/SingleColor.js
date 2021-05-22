2bf8c54e5400695f3f25f8dd9d0ff0d04c41de3e
pooya2293 <77061069+pooya2293@users.noreply.github.com>
2021-05-22 23:00:36 +0430

    join rgb arry with join(',')

--- 09-color-generator/setup/src/SingleColor.js
+++ 09-color-generator/setup/src/SingleColor.js
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from 'react'
 import rgbToHex from './utils'
 
 const SingleColor = ({ index,rgb,weight }) => {
+	const bcg = rgb.join(',')
+	console.log(rgb)//[255, 255, 255] //[253, 253, 253]
+	console.log(bcg)//255,255,255 //253,253,253
   return <h4>single color</h4>
 }
 
