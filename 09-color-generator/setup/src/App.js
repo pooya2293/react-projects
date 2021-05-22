3a286137d344ee1c112243ad0f7d9d9dc55e7653
pooya2293 <77061069+pooya2293@users.noreply.github.com>
2021-05-22 14:52:41 +0430

    color='' , error=false , list= [] , 2 section: 1 for input & 2 for list of colors

--- 09-color-generator/setup/src/App.js
+++ 09-color-generator/setup/src/App.js
@@ -4,7 +4,27 @@ import SingleColor from './SingleColor'
 import Values from 'values.js'
 
 function App() {
-  return <h2>color generator project</h2>
+	const [color,setColor] = useState('');
+	const [error,setError] = useState(false);
+	const [list,setList] = useState([]);
+
+	const handelSubmit = (e)=>{
+		e.preventDefault();
+		console.log('hello');
+	}
+  return (
+  	<>
+  		<section className='container' >
+  			<h3>colors genarator</h3>
+  			<form onSubmit={handelSubmit}>
+  				<input type="text" value={color} onChange={(e)=>setColor(e.target.value)}/>
+  			</form>
+  		</section>
+  		<section className="colors">
+  			<h4>List goes here</h4>
+  		</section>
+  	</>
+  	)
 }
 
 export default App
