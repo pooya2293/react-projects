cb18541cd37b2ce7d709e58d734e1e3a588e7198
pooya2293 <77061069+pooya2293@users.noreply.github.com>
2021-05-22 22:44:42 +0430

    giv path's from app.js

--- 09-color-generator/setup/src/SingleColor.js
+++ 09-color-generator/setup/src/SingleColor.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import rgbToHex from './utils'
 
-const SingleColor = () => {
+const SingleColor = ({ index,rgb,weight }) => {
   return <h4>single color</h4>
 }
 
