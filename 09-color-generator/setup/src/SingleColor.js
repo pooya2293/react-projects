7725cc037d6563310f209fa65a410404a8f543b8
pooya2293 <77061069+pooya2293@users.noreply.github.com>
2021-05-22 23:12:24 +0430

    add article with style background of: `rgb(${bcg})`

--- 09-color-generator/setup/src/SingleColor.js
+++ 09-color-generator/setup/src/SingleColor.js
@@ -5,7 +5,11 @@ const SingleColor = ({ index,rgb,weight }) => {
 	const bcg = rgb.join(',')
 	console.log(rgb)//[255, 255, 255] //[253, 253, 253]
 	console.log(bcg)//255,255,255 //253,253,253
-  return <h4>single color</h4>
+  return (
+  	<article className={`color`} style={{backgroundColor:`rgb(${bcg})`}}>
+  		single color
+  	</article>
+  )
 }
 
 export default SingleColor
