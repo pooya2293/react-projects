01a6f237901f2d86df2a6102445b9a75417805af
pooya2293 <77061069+pooya2293@users.noreply.github.com>
2021-05-23 13:07:41 +0430

    define hexVal for copy to clipboard btn

--- 09-color-generator/setup/src/SingleColor.js
+++ 09-color-generator/setup/src/SingleColor.js
@@ -5,11 +5,12 @@ const SingleColor = ({ index,rgb,weight,hexPaPa }) => {
 	const [alert,setAlert] = useState(false)//i did'nt understant
 	const bcg = rgb.join(',')
 	// const hex = rgbToHex(...rgb)//way 1 for get hex
+	const hexVal = `#${hexPaPa}`//u can use it for copy to clipboard btn
 	
   return (
   	<article className={`color ${index > 10 && 'color-light'}`} style={{backgroundColor:`rgb(${bcg})`}}>
   		<p className="percent-value">{weight}%</p>
-  		<p className='color-value'>#{hexPaPa}</p>
+  		<p className='color-value'>{hexVal}</p>
   	</article>
   )
 }
