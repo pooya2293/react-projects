57b61bba38d64b82a9a1a7d4523e5e66f96bdd2f
pooya2293 <77061069+pooya2293@users.noreply.github.com>
2021-05-23 11:30:20 +0430

    HP : geting hex i can't link hex to DOM

--- 09-color-generator/setup/src/SingleColor.js
+++ 09-color-generator/setup/src/SingleColor.js
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from 'react'
 import rgbToHex from './utils'
 
-const SingleColor = ({ index,rgb,weight }) => {
+const SingleColor = ({ index,rgb,weight,hex }) => {
 	const [alert,setAlert] = useState(false)//i did'nt understant
 	const bcg = rgb.join(',')
 	
   return (
   	<article className={`color`} style={{backgroundColor:`rgb(${bcg})`}}>
   		<p className="percent-value">{weight}%</p>
+  		<p className='color-value'>{hex}</p>
   	</article>
   )
 }
