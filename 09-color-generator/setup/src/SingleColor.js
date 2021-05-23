0ff7d4292c9a5844ea9ac1a811e38d9ce1a61eb8
pooya2293 <77061069+pooya2293@users.noreply.github.com>
2021-05-23 11:28:37 +0430

    alert = false , add weight , findout where come from : rgbToHex

--- 09-color-generator/setup/src/SingleColor.js
+++ 09-color-generator/setup/src/SingleColor.js
@@ -2,12 +2,12 @@ import React, { useState, useEffect } from 'react'
 import rgbToHex from './utils'
 
 const SingleColor = ({ index,rgb,weight }) => {
+	const [alert,setAlert] = useState(false)//i did'nt understant
 	const bcg = rgb.join(',')
-	console.log(rgb)//[255, 255, 255] //[253, 253, 253]
-	console.log(bcg)//255,255,255 //253,253,253
+	
   return (
   	<article className={`color`} style={{backgroundColor:`rgb(${bcg})`}}>
-  		single color
+  		<p className="percent-value">{weight}%</p>
   	</article>
   )
 }
