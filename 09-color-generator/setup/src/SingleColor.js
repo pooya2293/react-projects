21d3cf45c192db26155a2aa098c2f0c52bd262ba
pooya2293 <77061069+pooya2293@users.noreply.github.com>
2021-05-23 11:39:14 +0430

    SP: link hex: using of imported function

--- 09-color-generator/setup/src/SingleColor.js
+++ 09-color-generator/setup/src/SingleColor.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react'
 import rgbToHex from './utils'
 
-const SingleColor = ({ index,rgb,weight,hex }) => {
+const SingleColor = ({ index,rgb,weight }) => {
 	const [alert,setAlert] = useState(false)//i did'nt understant
 	const bcg = rgb.join(',')
+	const hex = rgbToHex(...rgb)//way 1 for get hex
 	
   return (
   	<article className={`color`} style={{backgroundColor:`rgb(${bcg})`}}>
