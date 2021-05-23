4d1d4a10a648b151b858d07e7cb7a72d96c95032
pooya2293 <77061069+pooya2293@users.noreply.github.com>
2021-05-23 13:37:18 +0430

    copy hexVal in clipboard But: it doesnt desipear

--- 09-color-generator/setup/src/SingleColor.js
+++ 09-color-generator/setup/src/SingleColor.js
@@ -2,15 +2,19 @@ import React, { useState, useEffect } from 'react'
 import rgbToHex from './utils'
 
 const SingleColor = ({ index,rgb,weight,hexPaPa }) => {
-	const [alert,setAlert] = useState(false)//i did'nt understant
+	const [alert,setAlert] = useState(false)//for showing copy to clipboard alert
 	const bcg = rgb.join(',')
 	// const hex = rgbToHex(...rgb)//way 1 for get hex
-	const hexVal = `#${hexPaPa}`//u can use it for copy to clipboard btn
+	const hexVal = `#${hexPaPa}`
 	
   return (
-  	<article className={`color ${index > 10 && 'color-light'}`} style={{backgroundColor:`rgb(${bcg})`}}>
+  	<article className={`color ${index > 10 && 'color-light'}`} style={{backgroundColor:`rgb(${bcg})`}} onClick={()=>{
+  		setAlert(true)
+  		navigator.clipboard.writeText(hexVal)
+  	}} >
   		<p className="percent-value">{weight}%</p>
   		<p className='color-value'>{hexVal}</p>
+  		{alert && <p className='alert'>copy to clipboard!</p>}
   	</article>
   )
 }
