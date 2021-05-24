a8d98e491856e92345eade899a1d1003be03abfb
pooya2293 <77061069+pooya2293@users.noreply.github.com>
2021-05-24 17:31:09 +0430

    if list empty do'nt show grocery-container: list.length > 0

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -34,10 +34,12 @@ function App() {
   			</button>
   		</div>
   	</form>
+  	{list.length > 0 && (
   		<div className="grocery-container">
   			<List items={list} />
   			<button className='clear-btn'>clear items</button>
   		</div>
+  	)}	
   	</section> 
   )
 }
