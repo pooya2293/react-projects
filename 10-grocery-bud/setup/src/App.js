f5178f60926bfc0f30abd8e2b35df2984fb6df3e
pooya2293 <77061069+pooya2293@users.noreply.github.com>
2021-05-26 10:54:52 +0430

    remove one item ,  style of alert rtl

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -38,6 +38,11 @@ const clearList = ()=>{
 	setList([])
 }
 
+const removeItem = (id)=>{
+	showAlert(true,'مورد موردنظر پاک شد ','danger')
+	setList(list.filter((item)=>item.id !== id))
+}
+
   return (
   	<section className="section-center">
   	<form className="grocery-form" onSubmit={handleSubmit} >
@@ -52,7 +57,7 @@ const clearList = ()=>{
   	</form>
   	{list.length > 0 && (
   		<div className="grocery-container">
-  			<List items={list} />
+  			<List items={list} removeItem={removeItem} />
   			<button className='clear-btn' onClick={()=>setDelAlert(true)} >clear items</button>
   		</div>
   	)}	
