86d7ad9bf20ed5a40390433748bb607f35fb69d7
pooya2293 <77061069+pooya2293@users.noreply.github.com>
2021-05-29 13:14:43 +0430

    vanila js getting location of elemnt in DOM

--- 11-navbar/setup/src/Navbar.js
+++ 11-navbar/setup/src/Navbar.js
@@ -5,7 +5,20 @@ import logo from './logo.svg'
 
 const Navbar = () => {
 	const[showLinks,setShowLinks] = useState(false)
+	const linksContainerRef = useRef(null)
+	const linksRef = useRef(null)
 
+	useEffect(() => {
+		const linksHeight = linksRef.current.getBoundingClientRect();
+		console.log(linksHeight)//bottom: 394.60071563720703
+								//height: 297.7430725097656
+								//left: 0
+								//right: 622.2222290039062
+								// top: 96.8576431274414
+								// width: 622.2222290039062
+								// x: 0
+								// y: 96.8576431274414
+	}, [showLinks])
   return (
   	<nav>
   		<div className="nav-center">
@@ -15,8 +28,8 @@ const Navbar = () => {
   					<FaBars />
   				</button>
   			</div>
-	  			<div className={`${showLinks?"links-container show-container": "links-container"}`}>
-	  				<ul className="links">
+	  			<div className="links-container" ref={linksContainerRef}>
+	  				<ul className="links" ref={linksRef}>
 	  					{links.map((link)=>{
 	  						const {id,url,text} = link
 	  						return( 
