b92871d5618a74642ce5102a2894e52d68e58b7f
pooya2293 <77061069+pooya2293@users.noreply.github.com>
2021-06-01 11:06:30 +0430

    use Context for link value from './context' to <Home />

--- 12-sidebar-modal/setup/src/Home.js
+++ 12-sidebar-modal/setup/src/Home.js
@@ -1,7 +1,10 @@
 import React, { useContext } from 'react'
 import { FaBars } from 'react-icons/fa'
-
+// #3
+import {AppContext} from './context'
 const Home = () => {
+	const data = useContext(AppContext)
+	console.log(data) //hello sagtoole
   return (
   	<main>
   		<button className='sidebar-toggle'><FaBars /></button>
