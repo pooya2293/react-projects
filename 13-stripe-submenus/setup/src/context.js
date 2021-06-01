9ab59a19e6b80dc5862ddc5a729607ed6f116792
pooya2293 <77061069+pooya2293@users.noreply.github.com>
2021-06-01 18:16:10 +0430

    <new course /> setup context and arrow function

--- 13-stripe-submenus/setup/src/context.js
+++ 13-stripe-submenus/setup/src/context.js
@@ -1,2 +1,39 @@
 import React, { useState, useContext } from 'react'
 import sublinks from './data'
+// #1
+const AppContext = React.createContext();
+
+export const AppProvider = ({children})=>{
+	const [isSidebarOpen,setIsSidebarOpen] = useState(true)
+	const [isSubmenuOpen,setIsSubmenuOpen] = useState(true)
+
+	const openSidebar =()=>{
+		setIsSidebarOpen(true)
+	}
+	const closeSidebar =()=>{
+		setIsSidebarOpen(false)
+	}
+	const openSubmenu =()=>{
+		setIsSubmenuOpen(true)
+	}
+	const closeSubmenu =()=>{
+		setIsSubmenuOpen(false)
+	}
+
+	return(
+		<AppContext.Provider value={{
+			isSidebarOpen,
+			isSubmenuOpen,
+			openSidebar,
+			closeSidebar,
+			openSubmenu,
+			closeSubmenu
+		}}>
+			{children}
+		</AppContext.Provider>
+	)
+}
+
+export const useGlobalContext =()=>{
+	return useContext(AppContext)
+}
