b0bc8b68a310b017c388f03e6fd5f657c90cc062
pooya2293 <77061069+pooya2293@users.noreply.github.com>
2021-06-02 14:14:33 +0430

    when hover show icon and label and change column with length of links get links from page:{page,links} come in from context

--- 13-stripe-submenus/setup/src/Submenu.js
+++ 13-stripe-submenus/setup/src/Submenu.js
@@ -7,17 +7,35 @@ const Submenu = () => {
 		location,
 		page:{page,links}} = useGlobalContext()
 	const container = useRef(null)
+	const [culomns,setColumns] = useState('col-2')
 	useEffect(() => {
 		const submenu = container.current
 		const {center,bottom} = location
 		submenu.style.left = `${center}px`
 		submenu.style.top = `${bottom}px`
 
-	}, [location])
+		if(links.length === 3) {
+			setColumns('col-3')
+		}
+		if(links.length > 3){
+			setColumns('col-4')
+		}
+
+	}, [location,links])
   return (
   	<aside className={`${isSubmenuOpen?'submenu show':'submenu'}`} ref={container} >
   		<h4>{page}</h4>
-
+  		<div className={`submenu-center ${culomns}`}>
+  			{links.map((link,index)=>{
+  				const{label,icon,url}=link
+  				return (
+  					<a href={url} key={index}>
+  						{icon}
+  						{label}
+  					</a>
+  				)
+  			})}
+  		</div>
   	</aside>	
   )
 }
