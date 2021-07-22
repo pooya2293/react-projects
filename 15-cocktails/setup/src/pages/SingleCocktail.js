bdb4f4ac2dd9fd22edff72259c2506b2ffa13cbf
pooya2293 <77061069+pooya2293@users.noreply.github.com>
2021-07-22 21:54:42 +0430

    when id change fecth id relate to the cocktail we click by url Api

--- 15-cocktails/setup/src/pages/SingleCocktail.js
+++ 15-cocktails/setup/src/pages/SingleCocktail.js
@@ -5,6 +5,21 @@ const url = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i='
 
 const SingleCocktail = () => {
 	const {id} = useParams();
+	const [lodaing,setLoading]=React.useState(false);
+	const [cocktail,setCocktail]=React.useState(null);
+
+	React.useEffect(()=>{
+		const newID = id.slice(1);//becuse id started by : slice first chracter
+		setLoading(true);
+		async function getCocktail(){
+			try {
+				const response = await fetch(`${url}${newID}`);
+				const data = await response.json();
+				console.log(data)
+			} catch(e) {}
+		}
+		getCocktail();
+	},[id]);
 
   return (
     <div>
