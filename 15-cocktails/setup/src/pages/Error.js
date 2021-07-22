ca4a717a0d51132067c11880b1c3bd02892df7ea
pooya2293 <77061069+pooya2293@users.noreply.github.com>
2021-07-22 09:04:40 +0430

    setup navbar error-page about page

--- 15-cocktails/setup/src/pages/Error.js
+++ 15-cocktails/setup/src/pages/Error.js
@@ -4,7 +4,15 @@ import { Link } from 'react-router-dom'
 const Error = () => {
   return (
     <div>
-      <h2>error page</h2>
+      <section className="error-page section">
+      	<div className="error-container">
+      		<h1>oops! its a dead end...</h1>
+      		<Link to='/' className='btn btn-primary'>
+      			Back Home
+      		</Link>
+      	</div>
+      	<h2>error page</h2>
+      </section>
     </div>
   )
 }
