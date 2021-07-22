aff3759769dc7724301cbb240dff54509ad15841
pooya2293 <77061069+pooya2293@users.noreply.github.com>
2021-07-22 11:10:44 +0430

    setup some context - show loading change height of animation for fun

--- 15-cocktails/setup/src/components/SearchForm.js
+++ 15-cocktails/setup/src/components/SearchForm.js
@@ -2,9 +2,10 @@ import React from 'react'
 import { useGlobalContext } from '../context'
 
 const SearchForm = () => {
+	const { setSearchTerm } = useGlobalContext();
   return (
     <div>
-      <h2>search form component</h2>
+    	<h2>search component</h2>
     </div>
   )
 }
