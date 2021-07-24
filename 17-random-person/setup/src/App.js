69f2660e21b528f8d1cdc718229dac348694bc3c
pooya2293 <77061069+pooya2293@users.noreply.github.com>
2021-07-24 22:05:49 +0430

    settup random person video:7:00

--- 17-random-person/setup/src/App.js
+++ 17-random-person/setup/src/App.js
@@ -10,7 +10,24 @@ import {
 const url = 'https://randomuser.me/api/'
 const defaultImage = 'https://randomuser.me/api/portraits/men/75.jpg'
 function App() {
-  return <h2>random user starter</h2>
+	const [loading,setLoading]=useState(true);
+	const [person,setPerson]=useState(null);
+	const [title,setTitle]=useState('name');
+	const [value,setValue]=useState('random person');
+
+	const handleValue=(e)=>{
+		console.log(e.target);
+	}
+  return(
+  	<main>
+  		<div className="block bcg-black"></div>
+  		<div className="block">
+  			<div className="container">
+  				<img src={(person&&person.image)||defaultImage} alt="random user" className='user-img' />
+  			</div>
+  		</div>
+  	</main>
+  )
 }
 
 export default App
