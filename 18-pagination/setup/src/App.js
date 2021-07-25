8e94d4f54352476f565f9bde0e137b429670e020
pooya2293 <77061069+pooya2293@users.noreply.github.com>
2021-07-25 18:03:48 +0430

    <new project /> 18-pagination

--- 18-pagination/setup/src/App.js
+++ 18-pagination/setup/src/App.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react'
 import { useFetch } from './useFetch'
 import Follower from './Follower'
 function App() {
+	const {loading,data}=useFetch();
+	//console.log(data);//show 100 data in array
   return <h2>pagination starter</h2>
 }
 
