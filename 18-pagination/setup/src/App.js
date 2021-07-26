d03ca0221a4416db6b868793abb15c7d8e5a18fd
pooya2293 <77061069+pooya2293@users.noreply.github.com>
2021-07-26 12:15:59 +0430

    show only when loading=T , loading is change show all in 0 data Array

--- 18-pagination/setup/src/App.js
+++ 18-pagination/setup/src/App.js
@@ -2,11 +2,14 @@ import React, { useState, useEffect } from 'react'
 import { useFetch } from './useFetch'
 import Follower from './Follower'
 function App() {
-	const {loading,data}=useFetch();
-  console.log(data);//Array(12)
-//0: (9) [{…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}]
-// 1: (9) [{…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}]
-// 2: (9) [{…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}]
+	const {loading , data} = useFetch();
+  const [page , setPage] = useState(0);
+  const [followers,setFollowers] = useState([]);
+
+  useEffect(()=>{
+    if(loading) return
+    setFollowers(data[page]);
+  },[loading])
 	
   return(
   	<main>
@@ -16,7 +19,7 @@ function App() {
   		</div>
   		<section className="followers">
   			<div className="container">
-  				{data.map((follower)=>{
+  				{followers.map((follower)=>{
   					return <Follower key={follower.id} {...follower} />
   				})}
   			</div>
