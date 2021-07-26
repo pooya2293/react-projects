35a9887dc09661f395020b93bee93f2bce3efb45
pooya2293 <77061069+pooya2293@users.noreply.github.com>
2021-07-26 20:43:56 +0430

    creat environmant variables i tell this in error.text helloWorld

--- 19-stock-photos/setup/src/App.js
+++ 19-stock-photos/setup/src/App.js
@@ -2,9 +2,10 @@ import React, { useState, useEffect } from 'react'
 import { FaSearch } from 'react-icons/fa'
 import Photo from './Photo'
 // const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
-//https://api.unsplash.com/photos/?client_id=AXB9LGz7FbtZ_LdC5gBA9EXJiaADX__-OKag1nX5Xlk
+
 const mainUrl = `https://api.unsplash.com/photos/`
 const searchUrl = `https://api.unsplash.com/search/photos/`
+const cliendID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
 
 
 function App() {
@@ -15,7 +16,7 @@ function App() {
 	const fetchData = async()=>{
 		setLoading(true)
 		let url;
-		url = `${mainUrl}?client_id=AXB9LGz7FbtZ_LdC5gBA9EXJiaADX__-OKag1nX5Xlk`
+		url = `${mainUrl}${cliendID}`
 		try{
 			const response = await fetch(url);
 			const data = await response.json();
