36e5c720e8c0917c4ec97f762dfb74403dc0f790
pooya2293 <77061069+pooya2293@users.noreply.github.com>
2021-07-26 10:46:21 +0430

    Back to standard stactcher and returns newFollowers

--- 18-pagination/setup/src/utils.js
+++ 18-pagination/setup/src/utils.js
@@ -1,15 +1,13 @@
 const paginate = (followers) => {
-	const itemsPerPage = 10;
+	const itemsPerPage = 9;
 	const pages = Math.ceil(followers.length / itemsPerPage);
-	console.log(pages);//ceil(10)=10
 
 	const newFollowers = Array.from({ length:pages },(_,index)=>{
 		const start = index * itemsPerPage;
-		console.log(start);//0,10,20,30,40,...,90
+
 		return followers.slice(start , start + itemsPerPage);
 	})
-	console.log(followers);//give all the followers data from <useFetch />
-	console.log(newFollowers);//(10) [Array(10), Array(10), Array(10), Array(10), Array(10), Array(10), Array(10), Array(10), Array(10), Array(10)]
+	return newFollowers;
 }
 
 export default paginate
