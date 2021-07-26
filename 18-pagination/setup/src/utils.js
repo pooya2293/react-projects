7939db743f4b146cbc30e6d016524d12962935a2
pooya2293 <77061069+pooya2293@users.noreply.github.com>
2021-07-26 10:42:55 +0430

    change itemsPerPage to 10

--- 18-pagination/setup/src/utils.js
+++ 18-pagination/setup/src/utils.js
@@ -1,15 +1,15 @@
 const paginate = (followers) => {
-	const itemsPerPage = 9;
+	const itemsPerPage = 10;
 	const pages = Math.ceil(followers.length / itemsPerPage);
-	console.log(pages);//ceil(11.22)=12
+	console.log(pages);//ceil(10)=10
 
 	const newFollowers = Array.from({ length:pages },(_,index)=>{
 		const start = index * itemsPerPage;
-		console.log(start);//0,9,18,27,36,45,...,99
+		console.log(start);//0,10,20,30,40,...,90
 		return followers.slice(start , start + itemsPerPage);
 	})
 	console.log(followers);//give all the followers data from <useFetch />
-	console.log(newFollowers);//(12) [Array(9), Array(9), Array(9), Array(9), Array(9), Array(9), Array(9), Array(9), Array(9), Array(9), Array(9), Array(1)]
+	console.log(newFollowers);//(10) [Array(10), Array(10), Array(10), Array(10), Array(10), Array(10), Array(10), Array(10), Array(10), Array(10)]
 }
 
 export default paginate
