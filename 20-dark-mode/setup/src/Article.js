958182d60d3997f13052e8b646cb0f76b9d22643
pooya2293 <77061069+pooya2293@users.noreply.github.com>
2021-07-28 18:17:14 +0430

    show date with momentjs but : i can't change locale to fa

--- 20-dark-mode/setup/src/Article.js
+++ 20-dark-mode/setup/src/Article.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import moment from 'moment'
+
 const Article = ({ date,length,snippet,title }) => {
+	console.log(moment.locale());//en
   return (
   	<article className='post'>
   		<h2>{title}</h2>
   		<div className='post-info'>
-  			<span>date</span>
+  			<span>{moment().format('LLLL')}</span>
   			<span>{length} min read</span>
   		</div>
   		<p>{snippet}</p>
