3c98799480b874d220da8d732e46f7fc4c51032e
pooya2293 <77061069+pooya2293@users.noreply.github.com>
2021-07-28 19:01:08 +0430

    when click toggle btn switch between light-theme and dark-theme But: still dont save this to locale storage

--- 20-dark-mode/setup/src/App.js
+++ 20-dark-mode/setup/src/App.js
@@ -3,12 +3,26 @@ import data from './data'
 import Article from './Article'
 
 function App() {
+	const [theme,setTheme] = useState('light-theme')
+
+	useEffect(()=>{
+		document.documentElement.className = theme
+	},[theme])
+
+	const toggle = ()=>{
+		if(theme === 'light-theme'){
+			setTheme('dark-theme')
+		}else{
+			setTheme('light-theme')
+		}
+	}
+
   return(
   	<main>
   		<nav>
   			<div className="nav-center">
   				<h1>overreacted</h1>
-  				<button className='btn'>toggle</button>
+  				<button className='btn' onClick={toggle} >toggle</button>
   			</div>
   		</nav>
   		<section className='articles'>
