a4b7f6387bbbd4969dbd5f4a127004cfb8a40313
pooya2293 <77061069+pooya2293@users.noreply.github.com>
2021-08-04 21:08:56 +0430

    show data in DOM

--- 22-hacker-news/setup/src/Stories.js
+++ 22-hacker-news/setup/src/Stories.js
@@ -3,13 +3,33 @@ import React from 'react'
 import { useGlobalContext } from './context'
 
 const Stories = () => {
-	const {isLoading} = useGlobalContext()
-	console.log(useGlobalContext())
+	const { isLoading,hits } = useGlobalContext()
+
 	if(isLoading){
 		return <div className="loading"></div>
 	}
 
-  return <h2>stories component</h2>
+  return (
+  	<section className="stories">
+  	  {hits.map((story)=>{
+  	  	const {objectID,title,url,num_comments,author,points} = story
+  	  	  return <article className="story" key={objectID}>
+  	  	    <h4 className="title">{title}</h4>
+  	  	    <p className="info">
+  	  	      {points} points by <span>{author} |</span> {num_comments} coments
+  	  	    </p>
+  	  	    <div>
+  	  	    	<a href={url} className='read_link' target='_blank'  rel="noopener noreferrer">
+  	  	    	  read more  
+  	  	    	</a>
+  	  	    	{' '}
+  	  	    	<button className="remove-btn">remove</button>
+  	  	    </div>
+  	  	    
+  	  	  </article>
+  	  })}
+  	</section>
+  )
 }
 
 export default Stories
