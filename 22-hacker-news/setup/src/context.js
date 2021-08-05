dc4cc79b38eb2dff074acfaf9a6320cdaffc9783
pooya2293 <77061069+pooya2293@users.noreply.github.com>
2021-08-05 14:47:39 +0430

    if click next and prev change page and load new page when isLoading true can not click button

--- 22-hacker-news/setup/src/context.js
+++ 22-hacker-news/setup/src/context.js
@@ -47,12 +47,12 @@ const AppProvider = ({ children }) => {
 	}
 
 	const handlePage = (value)=>{
-		console.log(value)
+		dispatch({ type:HANDLE_PAGE,payload:value })
 	}
 
 	useEffect(()=>{
 		fetchStories(`${API_ENDPOINT}query=${state.query}&page=${state.page}`)
-	},[state.query])
+	},[state.query,state.page])
 
   return <AppContext.Provider value={{ ...state,removeStory,searchQuery,handlePage }}>{children}</AppContext.Provider>
 }
